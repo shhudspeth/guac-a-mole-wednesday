f9a7b465c0cad3ded8eedc0b47bab7b71db8625f
Sarah Hudspeth <37939240+shhudspeth@users.noreply.github.com>
2020-10-29 09:03:32 -0700

    trying to get images to load

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -33,7 +33,7 @@ const Canvas = props => {
     console.log(score)
 
     // set source for gameoverbanner
-    const gameOverSource = ''https://shhudspeth.github.io/guac-a-mole-wednesday/Guacamole.gif'
+    const gameOverSource = ''https://shhudspeth.github.io/guac-a-mole-wednesday/public/Guacamole.gif'
 
     // SET ALL CHIPS TO DISPLAY #TRUE
     const [missed1, setMissed1] = useState(true);
@@ -68,8 +68,8 @@ const Canvas = props => {
     }
 
     const newBoard = new Image()
-    newBoard.src = 'https://shhudspeth.github.io/guac-a-mole-wednesday/cuttingboard.png'
-    const small_avocado = 'https://shhudspeth.github.io/guac-a-mole-wednesday/small_avocado.png'
+    newBoard.src = 'https://shhudspeth.github.io/guac-a-mole-wednesday/public/cuttingboard.png'
+    const small_avocado = 'https://shhudspeth.github.io/guac-a-mole-wednesday/public/small_avocado.png'
     const newAvocado = new Image()
     newAvocado.src = small_avocado
 
