10b2b579c5a538cc5e1e6eb357576ffaa6377fef
Sarah Hudspeth <37939240+shhudspeth@users.noreply.github.com>
2020-10-29 09:03:53 -0700

    Update Chip.js

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect, useState } from 'react'
 
 const Chip = props => {
     
-    const chipSource = 'https://shhudspeth.github.io/guac-a-mole-wednesday/tortilla.png'
+    const chipSource = 'https://shhudspeth.github.io/guac-a-mole-wednesday/public/tortilla.png'
     
     return <img src={chipSource} width="150px" height="150px"/>
 
